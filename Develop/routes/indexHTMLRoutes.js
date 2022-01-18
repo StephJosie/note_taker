7ab7a656d8ca5f76cba06f471f330d53315a0f34
steph <sjapodaca1103@gmail.com>
2022-01-17 16:48:14 -0800

    changed cons to make code easier to follow

--- Develop/routes/indexHTMLRoutes.js
+++ Develop/routes/indexHTMLRoutes.js
@@ -1,13 +1,13 @@
 const path = require('path');
-const htmlRoute = require('express').Router();
+const Routes = require('express').Router();
 
-htmlRoute.get('/notes', (req, res) => {
+Routes.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 
 });
-htmlRoute.get('*', (req, res) => {
+Routes.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 
 })
 
-module.exports = htmlRoute
+module.exports = Routes

bc41642dcfcee10747af858ee1a165941d4d0685
steph <sjapodaca1103@gmail.com>
2022-01-18 14:32:50 -0800

    adding to server.js/deleted a file

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const notesRoutes = require('./routes/notesRoutes')
-const indexHTMLRoutes = require('./routes/indexHTMLRoutes')
+// const notesRoutes = require('./routes/notesRoutes')
+// const indexHTMLRoutes = require('./routes/indexHTMLRoutes')
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+const readFileAsync = util.promisify(fs.readFile)
+const writeFileAsync = util.promisify(fs.writeFile)
+
 const app = express()
 const PORT = process.env.PORT || 3007
 
-app.use(express.json())
 
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', notesRoutes);
-app.use('/', indexHTMLRoutes);
-app.use(express.static('public'));
+app.use(express.json())
+// app.use('/api', notesRoutes);
+// app.use('/', indexHTMLRoutes);
+// app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT} `))
+
+
+
+
